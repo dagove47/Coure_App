d6f3d271dfc269a0ccd419afbc4109fea7997218
David Gómez <dagove47@gmail.com>
2023-10-25 01:20:33 -0600

    Sign up, log in and home added
    Sign up and log in functionality added

--- app/__init__.py
+++ app/__init__.py
@@ -1,41 +1,89 @@
-from flask import Flask, render_template, jsonify
-from config import DB_CONNECTION_STRING
-# from Course_App import app
+from flask import Flask, render_template, request, redirect, url_for, session
+from config import DB_CONNECTION_STRING, SECRET_KEY
 import cx_Oracle
 
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
 
-# Configuración de la conexión a la base de datos Oracle
 conn = cx_Oracle.connect(DB_CONNECTION_STRING)
 
-# print("************* TEST ---------")
+cursor = conn.cursor()
+
 
 @app.route('/')
 def index():
-    # Aquí puedes realizar consultas a la base de datos Oracle
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM NOMBRES')
-    data = cursor.fetchall()
-    cursor.close()
-
-    return render_template('index.html', data=data)
-
-@app.route('/get_names')
-def get_names():
-    cursor = conn.cursor()
-    cursor.execute("SELECT NOMBRE FROM NOMBRES")
-    data = cursor.fetchall()
-    cursor.close()
-
-    return jsonify(data)
-
-@app.route('/signin')
-def signin():
-    return render_template('signin.html')
+    return render_template('index.html')
 
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        cursor.execute("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)",
+                       {"username": username, "email": email, "password": password})
+        conn.commit()
+        return redirect(url_for('login'))
+    return render_template('signup.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        cursor.execute("SELECT * FROM users WHERE username = :username AND password = :password",
+                       {"username": username, "password": password})
+        user = cursor.fetchone()
+
+        if user:
+            session['user_id'] = user[0]
+            return redirect(url_for('home'))
+        else:
+            error = "Invalid credentials. Please try again."
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
 
 
+@app.route('/home')
+def home():
+    if 'user_id' in session:
+        return render_template('home.html')
+    else:
+        return redirect(url_for('login'))
 
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
+
+
+# print("************* TEST ---------")
+
+# @app.route('/')
+# def index():
+#     # Aquí puedes realizar consultas a la base de datos Oracle
+#     cursor = conn.cursor()
+#     cursor.execute('SELECT * FROM NOMBRES')
+#     data = cursor.fetchall()
+#     cursor.close()
+
+#     return render_template('index.html', data=data)
+
+# @app.route('/get_names')
+# def get_names():
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT NOMBRE FROM NOMBRES")
+#     data = cursor.fetchall()
+#     cursor.close()
+
+#     return jsonify(data)
+
+# @app.route('/signin')
+# def signin():
+#     return render_template('signin.html')
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,19 +1,84 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Make an AJAX request to fetch the data from the server
-    fetch('/get_names')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            const tableBody = document.getElementById('tableBody');
-            data.forEach(row => {
-                const newRow = document.createElement('tr');
-                const nameCell = document.createElement('td');
-                nameCell.textContent = row[0];  // Assuming the name is in the first column
-                newRow.appendChild(nameCell);
-                tableBody.appendChild(newRow);
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-});
+// function validateSignupForm() {
+//     var username = document.getElementById("username").value;
+//     var email = document.getElementById("email").value;
+//     var password = document.getElementById("password").value;
+
+//     // Check if all fields are required
+//     if (username === "" || email === "" || password === "") {
+//         alert("All fields are required!");
+//         return false;
+//     }
+
+//     // Validate the username and email addresses
+//     var regex = new RegExp("^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+$");
+//     if (!regex.test(email)) {
+//         alert("Invalid email address!");
+//         return false;
+//     }
+
+//     // Validate the password length
+//     if (password.length < 8) {
+//         alert("Password must be at least 8 characters long!");
+//         return false;
+//     }
+
+//     // All fields are valid!
+//     return true;
+// }
+
+// // Validate the log in form
+// function validateLoginForm() {
+//     var username = document.getElementById("username").value;
+//     var password = document.getElementById("password").value;
+
+//     // Check if all fields are required
+//     if (username === "" || password === "") {
+//         alert("All fields are required!");
+//         return false;
+//     }
+
+//     // All fields are valid!
+//     return true;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Make an AJAX request to fetch the data from the server
+//     fetch('/get_names')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//             const tableBody = document.getElementById('tableBody');
+//             data.forEach(row => {
+//                 const newRow = document.createElement('tr');
+//                 const nameCell = document.createElement('td');
+//                 nameCell.textContent = row[0];  // Assuming the name is in the first column
+//                 newRow.appendChild(nameCell);
+//                 tableBody.appendChild(newRow);
+//             });
+//         })
+//         .catch(error => {
+//             console.error('Error fetching data:', error);
+//         });
+// });
